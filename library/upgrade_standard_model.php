a0eca9c3ea33493168376dce21872a5598887a73
Wing Ming Chan <chanw@upstate.edu>
2016-10-14 10:30:00 -0400

    Initial commit

--- library/upgrade_standard_model.php
+++ library/upgrade_standard_model.php
@@ -23,78 +23,78 @@ use cascade_ws_exception as e;
 
 try
 {
-	$formats_folder = $cascade->getAsset(
-		a\Folder::TYPE, "formats", $site_name
-	);
-	$velocity_folder = $cascade->getAsset(
-		a\Folder::TYPE, "formats/library/velocity", $site_name
-	);
-	
-	$library_format_names = array(
-		"chanw_database_utilities",
-		"chanw_global_deque",
-		"chanw_global_queue",
-		"chanw_global_stack",
-		"chanw_global_utility_objects",
-		"chanw_global_values",
-		"chanw_global_values",
-		"chanw_global_values_code",   // xml block
-		"chanw_global_velocity_code", // xml block
-		"chanw_html_builder",
-		"chanw_html_builder_with_deque",
-		"chanw_library_import",
-		"chanw_object_creator",
-		"chanw_process_index_block",
-		"chanw_process_xml",
-		"chanw_reflect_utilities",
-		"chanw_sorted_pages",
-		"chanw_structured_data_worker",
-		"upstate_database"
-	);
+    $formats_folder = $cascade->getAsset(
+        a\Folder::TYPE, "formats", $site_name
+    );
+    $velocity_folder = $cascade->getAsset(
+        a\Folder::TYPE, "formats/library/velocity", $site_name
+    );
     
-	foreach( $library_format_names as $library_format_name )
-	{
-		if( u\StringUtility::endsWith( $library_format_name, "_code" ) )
-		{
-			$asset = $cascade->getAsset( 
-			    a\XmlBlock::TYPE,
-				$velocity_folder->getPath() . "/" . $library_format_name,
-				$site_name );
-			$block = true;
-		}
-		else
-		{
-			$asset = $cascade->getAsset( 
-			    a\ScriptFormat::TYPE,
-				$velocity_folder->getPath() . "/" . $library_format_name,
-				$site_name );
-			$block = false;
-		}
+    $library_format_names = array(
+        "chanw_database_utilities",
+        "chanw_global_deque",
+        "chanw_global_queue",
+        "chanw_global_stack",
+        "chanw_global_utility_objects",
+        "chanw_global_values",
+        "chanw_global_values",
+        "chanw_global_values_code",   // xml block
+        "chanw_global_velocity_code", // xml block
+        "chanw_html_builder",
+        "chanw_html_builder_with_deque",
+        "chanw_library_import",
+        "chanw_object_creator",
+        "chanw_process_index_block",
+        "chanw_process_xml",
+        "chanw_reflect_utilities",
+        "chanw_sorted_pages",
+        "chanw_structured_data_worker",
+        "upstate_database"
+    );
+    
+    foreach( $library_format_names as $library_format_name )
+    {
+        if( u\StringUtility::endsWith( $library_format_name, "_code" ) )
+        {
+            $asset = $cascade->getAsset( 
+                a\XmlBlock::TYPE,
+                $velocity_folder->getPath() . "/" . $library_format_name,
+                $site_name );
+            $block = true;
+        }
+        else
+        {
+            $asset = $cascade->getAsset( 
+                a\ScriptFormat::TYPE,
+                $velocity_folder->getPath() . "/" . $library_format_name,
+                $site_name );
+            $block = false;
+        }
 
-		$source_path    = $source . $library_format_name . ".xml";
-		$source_content = file_get_contents( $source_path );
+        $source_path    = $source . $library_format_name . ".xml";
+        $source_content = file_get_contents( $source_path );
 
-		if( $source_content != "" )
-		{
-			if( $block )
-			{
-				$source_content = str_replace( 
-				    "_common_assets", $site_name, $source_content );
-				$asset->setXml( $source_content )->edit();
-			}
-			else
-			{
-				$source_content = str_replace( "<code>", "", $source_content );
-				$source_content = str_replace( "</code>", "", $source_content );
-				$source_content = str_replace( "&lt;", "<", $source_content );
-				$source_content = str_replace( "&gt;", ">", $source_content );
-				$source_content = str_replace( "&amp;", "&", $source_content );
-				$source_content = str_replace( 
-				    "_common_assets", $site_name, $source_content );
-				$asset->setScript( $source_content )->edit();
-			}
-		}
-	}
+        if( $source_content != "" )
+        {
+            if( $block )
+            {
+                $source_content = str_replace( 
+                    "_common_assets", $site_name, $source_content );
+                $asset->setXml( $source_content )->edit();
+            }
+            else
+            {
+                $source_content = str_replace( "<code>", "", $source_content );
+                $source_content = str_replace( "</code>", "", $source_content );
+                $source_content = str_replace( "&lt;", "<", $source_content );
+                $source_content = str_replace( "&gt;", ">", $source_content );
+                $source_content = str_replace( "&amp;", "&", $source_content );
+                $source_content = str_replace( 
+                    "_common_assets", $site_name, $source_content );
+                $asset->setScript( $source_content )->edit();
+            }
+        }
+    }
 }
 catch( \Exception $e ) 
 {
