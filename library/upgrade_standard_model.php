8d178a947516134d33818dbaa47c3cf37f718586
Wing Ming Chan <chanw@upstate.edu>
2016-11-18 10:50:18 -0500

    Updated with new code for annotations

--- library/upgrade_standard_model.php
+++ library/upgrade_standard_model.php
@@ -24,7 +24,7 @@ use cascade_ws_exception as e;
 
 try
 {
-	// folder paths
+    // folder paths
     $formats_folder = $cascade->getAsset(
         a\Folder::TYPE, "formats", $site_name
     );
@@ -40,21 +40,21 @@ try
     );
     
     $formats_to_be_deleted = array(
-    	"chanw_global_utility_objects",
+        "chanw_global_utility_objects",
     );
     
     foreach( $blocks_to_be_deleted as $block_to_be_deleted )
     {
-    	$cascade->deleteXmlBlock(
-    		$velocity_folder->getPath() . "/" . $block_to_be_deleted,
-    		$site_name );
+        $cascade->deleteXmlBlock(
+            $velocity_folder->getPath() . "/" . $block_to_be_deleted,
+            $site_name );
     }
     
     foreach( $formats_to_be_deleted as $format_to_be_deleted )
     {
-    	$cascade->deleteScriptFormat(
-    		$velocity_folder->getPath() . "/" . $format_to_be_deleted,
-    		$site_name );
+        $cascade->deleteScriptFormat(
+            $velocity_folder->getPath() . "/" . $format_to_be_deleted,
+            $site_name );
     }
     
     $library_format_names = array(
@@ -84,46 +84,46 @@ try
     // create blocks needed and push the contents
     foreach( $library_block_names as $library_block_name )
     {
-		try
-		{
-			$block = $cascade->getAsset( 
-				a\XmlBlock::TYPE,
-				$velocity_folder->getPath() . "/" . $library_block_name,
-				$site_name );
-		}
-		catch( e\NullAssetException $e )
-		{
-			$block = $cascade->createXmlBlock(
-				$velocity_folder,
-				$library_block_name,
-				"<code></code>"
-			);
-		}
-    	$source_path = $source . $library_block_name . ".xml";
-		pushSource( $site_name, $block, $source_path );
-	}
+        try
+        {
+            $block = $cascade->getAsset( 
+                a\XmlBlock::TYPE,
+                $velocity_folder->getPath() . "/" . $library_block_name,
+                $site_name );
+        }
+        catch( e\NullAssetException $e )
+        {
+            $block = $cascade->createXmlBlock(
+                $velocity_folder,
+                $library_block_name,
+                "<code></code>"
+            );
+        }
+        $source_path = $source . $library_block_name . ".xml";
+        pushSource( $site_name, $block, $source_path );
+    }
 
     // create formats needed and push the contents
     foreach( $library_format_names as $library_format_name )
     {
-		try
-		{
-			$format = $cascade->getAsset( 
-				a\ScriptFormat::TYPE,
-				$velocity_folder->getPath() . "/" . $library_format_name,
-				$site_name );
-		}
-		catch( e\NullAssetException $e )
-		{
-			$format = $cascade->createScriptFormat(
-				$velocity_folder,
-				$library_format_name,
-				"##"
-			);
-		}
-		$source_path = $source . $library_format_name . ".xml";
-		pushSource( $site_name, $format, $source_path );
-	}
+        try
+        {
+            $format = $cascade->getAsset( 
+                a\ScriptFormat::TYPE,
+                $velocity_folder->getPath() . "/" . $library_format_name,
+                $site_name );
+        }
+        catch( e\NullAssetException $e )
+        {
+            $format = $cascade->createScriptFormat(
+                $velocity_folder,
+                $library_format_name,
+                "##"
+            );
+        }
+        $source_path = $source . $library_format_name . ".xml";
+        pushSource( $site_name, $format, $source_path );
+    }
 }
 catch( \Exception $e ) 
 {
@@ -136,29 +136,29 @@ catch( \Error $er )
 
 function pushSource( $site_name, $asset, $source_path )
 {
-	$source_content = file_get_contents( $source_path );
+    $source_content = file_get_contents( $source_path );
 
-	if( $source_content != "" )
-	{
-		$type = $asset->getType();
-		
-		if( $type == a\XmlBlock::TYPE )
-		{
-			$source_content = str_replace( 
-				"_common_assets", $site_name, $source_content );
-			$asset->setXml( $source_content )->edit();
-		}
-		else
-		{
-			$source_content = str_replace( "<code>", "", $source_content );
-			$source_content = str_replace( "</code>", "", $source_content );
-			$source_content = str_replace( "&lt;", "<", $source_content );
-			$source_content = str_replace( "&gt;", ">", $source_content );
-			$source_content = str_replace( "&amp;", "&", $source_content );
-			$source_content = str_replace( 
-				"_common_assets", $site_name, $source_content );
-			$asset->setScript( $source_content )->edit();
-		}
-	}
+    if( $source_content != "" )
+    {
+        $type = $asset->getType();
+        
+        if( $type == a\XmlBlock::TYPE )
+        {
+            $source_content = str_replace( 
+                "_common_assets", $site_name, $source_content );
+            $asset->setXml( $source_content )->edit();
+        }
+        else
+        {
+            $source_content = str_replace( "<code>", "", $source_content );
+            $source_content = str_replace( "</code>", "", $source_content );
+            $source_content = str_replace( "&lt;", "<", $source_content );
+            $source_content = str_replace( "&gt;", ">", $source_content );
+            $source_content = str_replace( "&amp;", "&", $source_content );
+            $source_content = str_replace( 
+                "_common_assets", $site_name, $source_content );
+            $asset->setScript( $source_content )->edit();
+        }
+    }
 }
 ?>
